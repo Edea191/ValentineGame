5f317937431b0811b86c2bcb4157e7764c2333ce
Edea191 <deswid@interia.pl>
2022-02-11 18:05:44 +0100

    edit game desing

--- init.js
+++ init.js
@@ -56,11 +56,11 @@ let activeStar ="rgb(255, 187, 0)";
 let InAtiveStar ="rgb(238, 222, 177)";
 
                     // Winner Point Setting //
-const topWinner = [60];
-const secondPlace = [50, 59];
-const thirdPlace = [39, 40];            // [x, y] -  x: min. points  y:max points 
-const fourthPlace = [29, 30];
-const fivesPlace = [19, 20];
+const topWinner = [75];
+const secondPlace = [60, 59];
+const thirdPlace = [45, 40];            // [x, y] -  x: min. points  y:max points 
+const fourthPlace = [30, 30];
+const fivesPlace = [15, 20];
 
 
 /////////////////////////////////////////////////////////////////////////////////
